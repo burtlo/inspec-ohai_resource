cf566dfbd7aa982658e2a72099a54bba397de41b
Lynn Frank <franklin.webber@gmail.com>
2019-03-23 16:12:28 -0500

    Uncomments other resources
    commented them out to increase speed of testing other work
    
    Signed-off-by: Franklin Webber <franklin.webber@gmail.com>

--- ohai/controls/example.rb
+++ ohai/controls/example.rb
@@ -1,33 +1,27 @@
 # encoding: utf-8
 # copyright: 2018, The Authors
 
-# describe ohai do
-#   its('uptime_seconds') { should be > 360 }
-#   its('os') { should eq 'darwin' }
+describe ohai do
+  # NOTE: If you stick with the inspec.json parsed solution this is what you 
+  #   live with when traversing elements in the results. I don't care for it much
+  # its( %w[chef_packages chef version ]) { should eq('14.11.21') }
+  its('chef_packages.chef.version') { should eq '14.11.21' }
+end
 
-# end
+describe elasticsearch_config('a.yml') do
+  its('xpack.security.authc.realms.pki1.type') { should eq('pki') }
+  its('xpack.security.authc.realms.pki1.order') { should eq(1) }
+end
 
-# describe ohai do
-#   # NOTE: If you stick with the inspec.json parsed solution this is what you 
-#   #   live with when traversing elements in the results. I don't care for it much
-#   # its( %w[chef_packages chef version ]) { should eq('14.11.21') }
-#   its('chef_packages.chef.version') { should eq '14.11.21' }
-# end
+describe elasticsearch_config('b.yml') do
+  its('xpack.security.authc.realms.pki1.type') { should eq('pki') }
+  its('xpack.security.authc.realms.pki1.order') { should eq(1) }
+end
 
-# describe elasticsearch_config('a.yml') do
-#   its('xpack.security.authc.realms.pki1.type') { should eq('pki') }
-#   its('xpack.security.authc.realms.pki1.order') { should eq(1) }
-# end
-
-# describe elasticsearch_config('b.yml') do
-#   its('xpack.security.authc.realms.pki1.type') { should eq('pki') }
-#   its('xpack.security.authc.realms.pki1.order') { should eq(1) }
-# end
-
-# describe elasticsearch_config('c.yml') do
-#   its('xpack.security.authc.realms.pki1.type') { should eq('pki') }
-#   its('xpack.security.authc.realms.pki1.order') { should eq(1) }
-# end
+describe elasticsearch_config('c.yml') do
+  its('xpack.security.authc.realms.pki1.type') { should eq('pki') }
+  its('xpack.security.authc.realms.pki1.order') { should eq(1) }
+end
 
 module Inspec::Resources
   class Directory
